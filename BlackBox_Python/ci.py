927f3614727e864c6cccc9a89448a825a7deec87
Vinver Guan <353164374@qq.com>
2018-03-02 20:48:13 -0800

    combine confidence.py and credible.py

--- BlackBox_Python/ci.py
+++ BlackBox_Python/ci.py
@@ -8,5 +8,18 @@ def getConfidenceInterval(df):
     Return:
     dataframe(three columns:name,lower_bound,upper_bound)
     """
-    
+
+    pass
+
+def getCredibleInterval(df):
+    """
+    compute 95% credible interval (bayesian approach)
+
+    Args:
+    dataframe: first column is name(string), second column is score(numeric)
+
+    Return:
+    dataframe(three columns:name,lower_bound,upper_bound)
+    """
+
     pass
