ff153a11ad246ba00306734584b5aef3a0a11c63
Vinver Guan <353164374@qq.com>
2018-03-02 20:17:54 -0800

    add getConfidenceInterval.py

---
+++ BlackBox_Python/getConfidenceInterval.py
@@ -0,0 +1,12 @@
+def getConfidenceInterval(df):
+    """
+    Compute 95% confidence interval (frequentist approach)
+
+    Args:
+    dataframe: first column is name(string), second column is score(numeric)
+
+    Return:
+    dataframe(three columns:name,lower_bound,upper_bound)
+    """
+    
+    pass
