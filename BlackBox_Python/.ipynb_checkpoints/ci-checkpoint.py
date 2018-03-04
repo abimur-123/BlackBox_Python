bb74564bbe2d01e87ee5d2136dc0ef3e2247cbf3
Mojo <abimur@gmail.com>
2018-03-04 01:51:26 -0800

    Add test and dummy functions

---
+++ BlackBox_Python/.ipynb_checkpoints/ci-checkpoint.py
@@ -0,0 +1,25 @@
+def getConfidenceInterval(df):
+    """
+    Compute 95% confidence interval (frequentist approach)
+
+    Args:
+    dataframe: first column is name(string), second column is score(numeric)
+
+    Return:
+    dataframe(three columns:name,lower_bound,upper_bound)
+    """
+
+    pass
+
+def getCredibleInterval(df):
+    """
+    compute 95% credible interval (bayesian approach)
+
+    Args:
+    dataframe: first column is name(string), second column is score(numeric)
+
+    Return:
+    dataframe(three columns:name,lower_bound,upper_bound)
+    """
+
+    pass
---
+++ BlackBox_Python/ABtests.py
@@ -0,0 +1,31 @@
+import pandas as pd
+def performABtest_Freq(df):
+    """
+    Get results of A/B tests done using the frequentist approach
+
+    Args:
+    dataframe: first column is event(string), second column is value(numeric)
+
+    Return:
+    p-value indicating significance.
+    """
+    if(isinstance(df,pd.DataFrame) == False):
+        raise TypeError("Not a data frame")
+    pass
+
+def performABtest_Bayesian(df,prior = None):
+    """
+    Get results of A/B tests done using the Bayesian approach
+
+    Args:
+    dataframe: first column is event(string), second column is value(numeric)
+    prior: prior assumptions of the data
+
+    Return:
+    p-value indicating significance.
+    """
+    if(isinstance(df,pd.DataFrame) == False):
+        raise TypeError("Not a data frame")
+
+    dummy_p_value = -0.5
+    return dummy_p_value
