c99609b5919b8a85b788f6afa2bb97e6e8a2fe4f
Mojo <abimur@gmail.com>
2018-03-10 12:37:36 -0800

    Add Python test cases

--- BlackBox_Python/ABtests.py
+++ BlackBox_Python/ABtests.py
@@ -1,6 +1,7 @@
 import scipy.stats as stats
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 import math
 
 def performABtest_Freq(data,alpha = 0.05):
@@ -8,57 +9,59 @@ def performABtest_Freq(data,alpha = 0.05):
     Get results of A/B tests done using the frequentist approach
 
     Args:
-    data: first column is event(string), second column is value(numeric)
-    alpha: false positive rate
+    data: dataframe with the events and the outcomes
+    alpha: False positive rate for the experiment
 
     Return:
-    1. p-value indicating significance
-    2. graph tracing p-values across runs
-    3. method used to compute p-value
+    1. p-value of the outcome
+    2. Graph indicating the variation of p-values
+    3. Method used to evaluate p-value
     """
 
-    #Tests
     if data is None or (not isinstance(data,pd.DataFrame)):
         raise TypeError("Input data must be a dataframe")
 
-    if (alpha > 1 | alpha < 0):
+    if alpha > 1 or alpha < 0:
         raise ValueError("Alpha/False positive rate cannot be greater than 1 or less than 0")
 
-    if df.shape[1] != 2:
+    if data.shape[1] != 2:
         raise TypeError("Input data must have 2 columns")
 
-    if (!np.array_equal(data.iloc[:,1].unique(),[0,1])):
+    if not np.array_equal(data["event"].sort_values().unique(),[0,1]):
         raise TypeError("Input events can have values of just 0 and 1")
 
-    if (len(np.unique(data.iloc[:,0])) != 2):
+    if (len(np.unique(data["input"])) != 2):
         raise TypeError("Can have only 2 events for the test")
 
-    # Construct contingency table(2 x 2 table)
+    if (len(data) < 20):
+        raise TypeError("Must have at least 20 rows in the dataframe")
+
+    data = data.sample(frac=1).reset_index(drop=True)
+
     conti_tab = data.groupby('input').agg({'event': ['sum', 'count']})
     conti_tab['event']['count'] = conti_tab['event']['count'] - conti_tab['event']['sum']
 
-    start_iter = floor(0.75 * len(inp))
+    start_iter = math.floor(0.75 * len(data))
 
-    #compute p-value for records
     ip = {'index':[],'p_val':[]}
     if len(data) < 2000:
-        method = "Fisher"
-        for i in range(start_iter,len(inp)):
-            conti_tab = inp[0:i].groupby('input').agg({'event': ['sum', 'count']})
+        method = "Fisher's exact test"
+        for i in range(start_iter,len(data)):
+            conti_tab = data[0:i].groupby('input').agg({'event': ['sum', 'count']})
             conti_tab['event']['count'] = conti_tab['event']['count'] - conti_tab['event']['sum']
             ip['index'].append(i)
             pval = stats.fisher_exact(conti_tab['event'])[1]
             ip['p_val'].append(round(pval,4))
     else:
         method = "Chi square test"
-        for i in range(start_iter,len(inp)):
-            conti_tab = inp[0:i].groupby('input').agg({'event': ['sum', 'count']})
+        for i in range(start_iter,len(data)):
+            conti_tab = data[0:i].groupby('input').agg({'event': ['sum', 'count']})
             conti_tab['event']['count'] = conti_tab['event']['count'] - conti_tab['event']['sum']
             ip['index'].append(i)
             pval = stats.chi2_contingency(conti_tab['event'])[1]
             ip['p_val'].append(round(pval,4))
 
-    f, ax = plt.subplots(figsize=(7,5))
+    f, ax = plt.subplots(figsize=(8,8))
     ax.plot(ip['index'],ip['p_val'], "-or")
     ax.axhline(y=alpha, color='g', linestyle='-')
     ax.set_xlabel("Record number")
--- BlackBox_Python/tests/performABTest.py
+++ BlackBox_Python/tests/performABTest.py
@@ -4,6 +4,7 @@ AB Tests(frequentist approach)
 import pytest
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 import sys
 import os
@@ -12,21 +13,20 @@ sys.path.insert(0, os.path.abspath("../"))
 
 from BlackBox_Python import ABtests as AB
 
-#sample dataframes
-n = 2
-p = 0.5
-x = 1
-name = np.repeat(('A','B'),n/2)
-value= np.random.binomial(x, p,size = n)
-data = {'name':name,'value':value}
-df=pd.DataFrame(data)
-
 #check input
 def test_input():
     '''
-    check if the input is valid for performABtest_Freq function
-    Try invalid and NULL input.
+    Checks if input for the functions are as expected
+    This should silently error
     '''
+    #sample dataframes
+    n = 2
+    p = 0.5
+    x = 1
+    name = np.repeat(('A','B'),n/2)
+    value= np.random.binomial(x, p,size = n)
+    data = {'name':name,'value':value}
+    data=pd.DataFrame(data)
 
     # Capture incorrect input data
     try:
@@ -47,6 +47,7 @@ def test_input():
 
     try:
         data = {'name':name,'value':value,'value_copy':value}
+        data=pd.DataFrame(data)
         AB.performABtest_Freq(data = data)
     except(TypeError):
         assert True
@@ -66,7 +67,37 @@ def test_input():
     else:
         False
 
-    # Generate output
+    #More than 2 kinds of event
+    try:
+        n = 20
+        p = 0.5
+        x = 1
+        name = np.repeat(('A','B','C'),n/3)
+        value= np.random.binomial(x, p,size = n)
+        data = {'name':name,'value':value}
+    except(TypeError):
+        assert True
+    else:
+        False
+
+    # Length of dataframes
+    try:
+        n = 18
+        d = {'input':('A','B') * (n//2),'event':np.random.binomial(1,0.5,n)}
+        inp = pd.DataFrame(data=d)
+        AB.performABtest_Freq(inp)
+    except(TypeError):
+        assert True
+    else:
+        False
+
+
+def test_output():
+    '''
+    This function checks if output of the function is as expected and does branch testing(white box testing)
+    '''
+
+    # Generate output. This covers branch with Chi-square test(Branch 1)
     n = 2500
     p = 0.5
     x = 1
@@ -74,20 +105,27 @@ def test_input():
     value= np.random.binomial(x, p,size = n)
     d = {'input':name,'event':value}
     inp = pd.DataFrame(data=d)
-    op = AB_freq(inp,0.1)
+    op = AB.performABtest_Freq(inp,0.1)
+    assert op[2] == "Chi square test"
+
+    # Generate output. This covers branch with Fisher's exact test(Branch 2)
+    n = 250
+    p = 0.5
+    x = 1
+    name = np.repeat(('A','B'),n/2)
+    value= np.random.binomial(x, p,size = n)
+    d = {'input':name,'event':value}
+    inp = pd.DataFrame(data=d)
+    op = AB.performABtest_Freq(inp,0.1)
+    assert op[2] == "Fisher's exact test"
 
     # Check if p-value is between 0 and 1
-    op_p_val = True if op[1] > 0 and op[1] <= 1 else False
-    
+    op_p_val = True if op[0] > 0 and op[0] <= 1 else False
+
     #assert length and type of op
     assert len(op) == 3
-    assert isinstance(data,pd.DataFrame) == True
+    assert isinstance(op,list) == True
     assert op_p_val == True
-
-def test_output():
-    '''
-    check if the output is in the valid range for the performABtest_Bayesian function
-    '''
 
     assert AB.performABtest_Bayesian(df) > 0
     assert AB.performABtest_Bayesian(df) < 1
