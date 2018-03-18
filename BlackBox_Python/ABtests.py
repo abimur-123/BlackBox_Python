f8eb5f2ed4122b1a3cf2e1f0d0ab629966a931fc
Mojo <abimur@gmail.com>
2018-03-18 13:48:16 -0700

    Comment out Bayesian AB

--- BlackBox_Python/ABtests.py
+++ BlackBox_Python/ABtests.py
@@ -71,19 +71,19 @@ def performABtest_Freq(data,alpha = 0.05):
 
     return [ip['p_val'][-1],ax,method]
 
-def performABtest_Bayesian(df,prior = None):
-    """
-    Get results of A/B tests done using the Bayesian approach
-
-    Args:
-    dataframe: first column is event(string), second column is value(numeric)
-    prior: prior assumptions of the data
-
-    Return:
-    p-value indicating significance.
-    """
-    if(isinstance(df,pd.DataFrame) == False):
-        raise TypeError("Not a data frame")
-
-    dummy_p_value = -0.5
-    return dummy_p_value
+# def performABtest_Bayesian(df,prior = None):
+#     """
+#     Get results of A/B tests done using the Bayesian approach
+#
+#     Args:
+#     dataframe: first column is event(string), second column is value(numeric)
+#     prior: prior assumptions of the data
+#
+#     Return:
+#     p-value indicating significance.
+#     """
+#     if(isinstance(df,pd.DataFrame) == False):
+#         raise TypeError("Not a data frame")
+#
+#     dummy_p_value = -0.5
+#     return dummy_p_value
