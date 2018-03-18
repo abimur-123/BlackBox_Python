408cd933e460dc39813082c0ceba419575ffcfb8
Mojo <abimur@gmail.com>
2018-03-18 14:34:11 -0700

    remove Bayesian from import

--- BlackBox_Python/__init__.py
+++ BlackBox_Python/__init__.py
@@ -1,3 +1,3 @@
 from BlackBox_Python.ci import getCredibleInterval, getConfidenceInterval
-from BlackBox_Python.ABtests import performABtest_Freq, performABtest_Bayesian
+from BlackBox_Python.ABtests import performABtest_Freq
 from BlackBox_Python.MapVMle import getMLE
