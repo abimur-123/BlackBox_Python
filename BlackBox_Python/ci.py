08e0eacfb85b4163559a7ed829ae7c1bac63e7d0
sarora <sidd_99@hotmail.com>
2018-03-18 01:22:05 -0700

    Add yin and sheikhs changes

--- BlackBox_Python/ci.py
+++ BlackBox_Python/ci.py
@@ -1,25 +1,63 @@
-def getConfidenceInterval(df):
+import numpy as np
+import scipy.stats as stats
+
+def getConfidenceInterval(x):
     """
     Compute 95% confidence interval (frequentist approach)
 
     Args:
-    dataframe: first column is name(string), second column is score(numeric)
+    x :numpy array, with at least 1 observation
 
     Return:
-    dataframe(three columns:name,lower_bound,upper_bound)
+    interval: list with 2 elements
     """
+    if (len(x.shape)>1):
+        raise AttributeError('Input data must be numpy array with one column only')
 
-    pass
+    if(isinstance(x,np.ndarray) == False):
+        raise TypeError("Not a numpy array")
 
-def getCredibleInterval(df):
+    assert x.shape[0]!=0, "Empty Array!"
+
+    xbar=x.mean()
+    n=x.shape[0]
+    sd=x.std()
+    lower=xbar-1.96*(sd/np.sqrt(n))
+    upper=xbar+1.96*(sd/np.sqrt(n))
+    interval=[lower,upper]
+    return interval
+
+
+def getCredibleInterval(x,prior_dis,sample_dis):
     """
     compute 95% credible interval (bayesian approach)
 
     Args:
-    dataframe: first column is name(string), second column is score(numeric)
+    x:numpy array with at least 1 observation
+    prior_dis: list, with exactly two number
+    sample_dis: list, with exactly two number
 
     Return:
-    dataframe(three columns:name,lower_bound,upper_bound)
+    interval: list with 2 elements
     """
+    if (len(x.shape)>1):
+        raise AttributeError('Input data must be numpy array with one column only')
+    if(isinstance(x,np.ndarray) == False):
+        raise TypeError("Not a numpy array")
+    assert x.shape[0]!=0, "Empty Array!"
 
-    pass
+    prior_mean=prior_dis[0]
+    prior_sd=prior_dis[1]
+    sample_mean=sample_dis[0]
+    sample_sd=sample_dis[1]
+    vector_mean=x.mean()
+    vector_n=x.shape[0]
+    post_mean=(vector_mean*vector_n/sample_sd+prior_mean*1/prior_sd)/(vector_n/sample_sd+1/prior_sd**2)
+    post_sd=1/(vector_n/(sample_sd**2)+1/(prior_sd**2))
+
+    lower=stats.norm.ppf(0.025,loc=post_mean,scale=post_sd)
+    upper=stats.norm.ppf(0.975,loc=post_mean,scale=post_sd)
+
+    interval=[lower,upper]
+
+    return interval
